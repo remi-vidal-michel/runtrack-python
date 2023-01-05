27170fc2d51e66d8dde0e81805d1d0c4da73b4bb
remi-vidal-michel <remi.vidal-michel@laplateforme.io>
2023-01-05 13:25:50 +0100

    [ADD] even nums sum

---
+++ jour04/job08.py
@@ -0,0 +1,6 @@
+L = [8, 24, 27, 48, 2, 16, 9, 7, 84, 91]
+somme = 0
+for i in L:
+    if i % 2 == 0:
+        somme+=i
+print("La somme de tous les nombres pairs présents dans la liste fait "+str(somme))
