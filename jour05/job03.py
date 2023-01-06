38dc6e6fbdb29032d23b9d023ce328cd68502f4c
remi-vidal-michel <remi.vidal-michel@laplateforme.io>
2023-01-06 13:39:07 +0100

    [FIX] turned program into function

--- jour05/job03.py
+++ jour05/job03.py
@@ -1,8 +1,10 @@
-w = (int(input("taille ? : ")))
-h = w
-j = 0
-print("+", "-"*(w+1), "+")
-for i in range(h+1):
-    print("|", "#"*(w-j), "#"*j, "|")
-    j+=1
-print("+", "-"*(w+1), "+")
+def draw_rectangle(w):
+    h = w
+    j = 0
+    print("+", "-"*(w+1), "+")
+    for i in range(h+1):
+        print("|", "#"*(w-j), "#"*j, "|")
+        j+=1
+    print("+", "-"*(w+1), "+")
+
+draw_rectangle(5)
